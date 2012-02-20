20b08eae9ff4b0d484bed70f45bd55184ba697e8
Dan Peterson <dpiddy@gmail.com>
2012-02-20 14:40:34 -0400

    Rework a little.

--- lib/gemgate/latest_specs.rb
+++ lib/gemgate/latest_specs.rb
@@ -1,9 +1,31 @@
 module Gemgate
   class LatestSpecs
+    class Entry
+      def initialize(gem)
+        @gem = gem
+      end
+
+      def for_inclusion
+        [@gem.name, @gem.version, @gem.platform]
+      end
+    end
+
     attr_accessor :storage
 
     def add(gem)
-      storage.update("latest_specs.4.8.gz", Gem.gzip(Marshal.dump([[gem.name, gem.version, gem.platform]])))
+      entry = Entry.new(gem)
+
+      storage.update(filename, to_gzipped_marshal([entry.for_inclusion]))
+    end
+
+    private
+
+    def filename
+      "latest_specs.4.8.gz"
+    end
+
+    def to_gzipped_marshal(data)
+      Gem.gzip(Marshal.dump(data))
     end
   end
 end
--- lib/gemgate/quick_marshal_specs.rb
+++ lib/gemgate/quick_marshal_specs.rb
@@ -1,9 +1,31 @@
 module Gemgate
   class QuickMarshalSpecs
+    class Entry
+      def initialize(gem)
+        @gem = gem
+      end
+
+      def filename
+        "quick/Marshal.4.8/#{@gem.spec_filename}.rz"
+      end
+
+      def spec
+        @gem.spec
+      end
+    end
+
     attr_accessor :storage
 
     def add(gem)
-      storage.create("quick/Marshal.4.8/#{gem.spec_filename}.rz", Gem.deflate(Marshal.dump(gem.spec)))
+      entry = Entry.new(gem)
+
+      storage.create(entry.filename, to_deflated_marshal(entry.spec))
+    end
+
+    private
+
+    def to_deflated_marshal(data)
+      Gem.deflate(Marshal.dump(data))
     end
   end
 end
