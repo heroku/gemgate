984e2febe08d27e81c42d35bc3726acbabfec12e
Dan Peterson <dpiddy@gmail.com>
2012-03-19 23:13:18 -0700

    uniq specs index data.
    Includes a hack to create our own instances of Gem::Version everywhere. This works around the @hash instance variable
    possibly being set (and dumped) in the existing marshal data or in the YAML metadata in the incoming gem. This was
    observed in a case where `gem build` was run under 1.8.7 but gemgate was running using 1.9.2.

--- lib/gemgate/specs_index.rb
+++ lib/gemgate/specs_index.rb
@@ -25,7 +25,11 @@ module Gemgate
     end
 
     def update_with(gem)
-      store(existing_data + [[gem.name, gem.version, gem.platform]])
+      # create our own instance of Gem::Version for the incoming gem version.
+      # this works around an issue where the @hash instance variable is already set
+      # in the YAML-dumped metadata in the uploaded gem, which breaks our uniq'ing of the
+      # index.
+      store(existing_data + [[gem.name, Gem::Version.create(gem.version.version), gem.platform]])
     end
 
     def existing_file_data
@@ -34,14 +38,17 @@ module Gemgate
 
     def existing_data
       if existing_file_data
-        from_gzipped_marshal(existing_file_data)
+        from_gzipped_marshal(existing_file_data).map do |(name, version, platform)|
+          # create our own Gem::Version here, too
+          [name, Gem::Version.create(version.version), platform]
+        end
       else
         []
       end
     end
 
     def store(data)
-      storage.update(@filename, to_gzipped_marshal(data))
+      storage.update(@filename, to_gzipped_marshal(data.uniq))
     end
 
     def to_gzipped_marshal(data)
--- spec/specs_index_spec.rb
+++ spec/specs_index_spec.rb
@@ -6,12 +6,12 @@ describe Gemgate::SpecsIndex do
   it "adds a gem using storage when there is no existing file" do
     gem = mock("gem")
     gem.should_receive(:name) { "foobar" }
-    gem.should_receive(:version) { "0.0.1" }
+    gem.should_receive(:version) { Gem::Version.create("0.0.1") }
     gem.should_receive(:platform) { "ruby" }
 
     storage = mock("storage")
     storage.should_receive(:get).with("foobar_specs.4.8.gz") { nil }
-    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["foobar", "0.0.1", "ruby"]])))
+    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["foobar", Gem::Version.create("0.0.1"), "ruby"]])))
 
     subject.storage = storage
 
@@ -21,12 +21,12 @@ describe Gemgate::SpecsIndex do
   it "adds a gem using storage when there is an existing file" do
     gem = mock("gem")
     gem.should_receive(:name) { "foobar" }
-    gem.should_receive(:version) { "0.0.1" }
+    gem.should_receive(:version) { Gem::Version.create("0.0.1") }
     gem.should_receive(:platform) { "ruby" }
 
     storage = mock("storage")
-    storage.should_receive(:get).with("foobar_specs.4.8.gz") { Gem.gzip(Marshal.dump([["something", "0.0.2", "ruby"]])) }
-    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["something", "0.0.2", "ruby"], ["foobar", "0.0.1", "ruby"]])))
+    storage.should_receive(:get).with("foobar_specs.4.8.gz") { Gem.gzip(Marshal.dump([["something", Gem::Version.create("0.0.2"), "ruby"]])) }
+    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["something", Gem::Version.create("0.0.2"), "ruby"], ["foobar", Gem::Version.create("0.0.1"), "ruby"]])))
 
     subject.storage = storage
 
@@ -34,21 +34,21 @@ describe Gemgate::SpecsIndex do
   end
 
   it "adds if all provided conditions are true" do
-    gem = stub("gem", :name => "foobar", :version => "0.0.1", :platform => "ruby")
+    gem = stub("gem", :name => "foobar", :version => Gem::Version.create("0.0.1"), :platform => "ruby")
 
     storage = mock("storage")
     storage.should_receive(:get).with("foobar_specs.4.8.gz") { nil }
-    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["foobar", "0.0.1", "ruby"]])))
+    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["foobar", Gem::Version.create("0.0.1"), "ruby"]])))
 
     subject.storage = storage
 
-    subject.conditions << lambda {|g| g.name == "foobar" } << lambda {|g| g.version == "0.0.1" }
+    subject.conditions << lambda {|g| g.name == "foobar" } << lambda {|g| g.version == Gem::Version.create("0.0.1") }
 
     subject.add(gem)
   end
 
   it "skips adding if not all provided conditions are true" do
-    gem = stub("gem", :name => "foobar", :version => "0.0.1", :platform => "ruby")
+    gem = stub("gem", :name => "foobar", :version => Gem::Version.create("0.0.1"), :platform => "ruby")
 
     storage = mock("storage")
     storage.should_receive(:get).with("foobar_specs.4.8.gz") { Gem.gzip(Marshal.dump([])) }
@@ -62,7 +62,7 @@ describe Gemgate::SpecsIndex do
   end
 
   it "ensures the file exists even when not adding" do
-    gem = stub("gem", :name => "foobar", :version => "0.0.1", :platform => "ruby")
+    gem = stub("gem", :name => "foobar", :version => Gem::Version.create("0.0.1"), :platform => "ruby")
 
     storage = mock("storage")
     storage.should_receive(:get).with("foobar_specs.4.8.gz") { nil }
@@ -70,7 +70,22 @@ describe Gemgate::SpecsIndex do
 
     subject.storage = storage
 
-    subject.conditions << lambda {|g| g.name == "foobar" } << lambda {|g| g.version == "0.0.2" }
+    subject.conditions << lambda {|g| g.name == "foobar" } << lambda {|g| g.version == Gem::Version.create("0.0.2") }
+
+    subject.add(gem)
+  end
+
+  it "uniqs the added data" do
+    gem = mock("gem")
+    gem.should_receive(:name) { "foobar" }
+    gem.should_receive(:version) { Gem::Version.create("0.0.1") }
+    gem.should_receive(:platform) { "ruby" }
+
+    storage = mock("storage")
+    storage.should_receive(:get).with("foobar_specs.4.8.gz") { Gem.gzip(Marshal.dump([["foobar", Gem::Version.create("0.0.1"), "ruby"]])) }
+    storage.should_receive(:update).with("foobar_specs.4.8.gz", Gem.gzip(Marshal.dump([["foobar", Gem::Version.create("0.0.1"), "ruby"]])))
+
+    subject.storage = storage
 
     subject.add(gem)
   end
